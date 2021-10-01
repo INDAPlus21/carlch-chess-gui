280b43b1e583b0dca238e5633dc69813c669cad9
Chemnitz <carlch@kth.se>
2021-10-01 15:53:23 +0200

    Initialize project

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use ggez::event;
+use ggez::graphics::{self, DrawParam, Color, DrawMode};
+use ggez::{Context, GameResult};
+use std::path;
+use chess_template::{Game, GameState, Colour, PieceType};
+use ggez::event::{MouseButton};
+
+fn main() {
+    println!("Hello, world!");
+}
